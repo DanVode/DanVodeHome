02a63c5bbc46bf472eb47042458a2bce2fa8dfeb
Devindel2 <82938160+Devindel2@users.noreply.github.com>
2021-10-08 14:38:45 +0300

    Last version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,16 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Fuctorial(8));
+        System.out.println(fuctorial(8));
 
     }
 
-    public static int Fuctorial(int a){
+    public static int fuctorial(int a){
 
         if (a == 1) {
             return 1;
         }
 
-        return a*Fuctorial(a-1);
+        return a*fuctorial(a-1);
     }
 }
