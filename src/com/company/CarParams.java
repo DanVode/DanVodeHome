7090fbfc2038917584acaef20e372527a7a2885a
Devindel2 <82938160+Devindel2@users.noreply.github.com>
2021-10-18 21:23:20 +0300

    Test 2

--- src/com/company/CarParams.java
+++ src/com/company/CarParams.java
@@ -1,21 +1,26 @@
 package com.company;
 
-import java.util.Objects;
 
 public class CarParams {
 
     public static void main(String... args) {
-        Cars firstCar = new Cars(100, 120, 2000);
+        Cars firstCar = new Cars(110, 120, 2000);
         Cars secondCar  = new Cars(110, 140, 2400);
 
 
         System.out.println(firstCar.equals(secondCar));
     }
 
+
+
     static class Cars {
         int engine;
         int maxSpeed;
         int weight;
+
+        public boolean equals(Cars cars) {
+            return this.maxSpeed == cars.maxSpeed;
+        }
 
         public Cars(int engine, int weight, int maxSpeed) {
             this.engine = engine;
@@ -23,17 +28,17 @@ public class CarParams {
             this.maxSpeed = maxSpeed;
         }
 
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Cars cars = (Cars) o;
-            return engine == cars.engine && maxSpeed == cars.maxSpeed && weight == cars.weight;
-        }
+//        @Override
+//        public boolean equals(Object o) {
+//            if (this == o) return true;
+//            if (o == null || getClass() != o.getClass()) return false;
+//            Cars cars = (Cars) o;
+//            return engine == cars.engine && maxSpeed == cars.maxSpeed && weight == cars.weight;
+//        }
 
-        @Override
-        public int hashCode() {
-            return Objects.hash(engine, maxSpeed, weight);
-        }
+//        @Override
+//        public int hashCode() {
+//            return Objects.hash(engine, maxSpeed, weight);
+//        }
     }
 }
