0e479805d965b9408585fed65a4040406f8e5c54
Devindel2 <82938160+Devindel2@users.noreply.github.com>
2021-10-18 22:04:09 +0300

    Test 3

--- src/com/company/CarParams.java
+++ src/com/company/CarParams.java
@@ -1,14 +1,18 @@
 package com.company;
 
+import java.util.Objects;
 
 public class CarParams {
 
     public static void main(String... args) {
-        Cars firstCar = new Cars(110, 120, 2000);
+        Cars firstCar = new Cars(100, 120, 20000);
         Cars secondCar  = new Cars(110, 140, 2400);
 
-
-        System.out.println(firstCar.equals(secondCar));
+        if (secondCar.maxSpeed > firstCar.maxSpeed){
+            System.out.println("Вторая машина быстрее");
+        } else {
+            System.out.println("Первая машина быстрее");
+        }
     }
 
 
@@ -18,27 +22,23 @@ public class CarParams {
         int maxSpeed;
         int weight;
 
-        public boolean equals(Cars cars) {
-            return this.maxSpeed == cars.maxSpeed;
-        }
-
         public Cars(int engine, int weight, int maxSpeed) {
             this.engine = engine;
             this.weight = weight;
             this.maxSpeed = maxSpeed;
         }
 
-//        @Override
-//        public boolean equals(Object o) {
-//            if (this == o) return true;
-//            if (o == null || getClass() != o.getClass()) return false;
-//            Cars cars = (Cars) o;
-//            return engine == cars.engine && maxSpeed == cars.maxSpeed && weight == cars.weight;
-//        }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Cars cars = (Cars) o;
+            return engine == cars.engine && maxSpeed == cars.maxSpeed && weight == cars.weight;
+        }
 
-//        @Override
-//        public int hashCode() {
-//            return Objects.hash(engine, maxSpeed, weight);
-//        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(engine, maxSpeed, weight);
+        }
     }
 }
